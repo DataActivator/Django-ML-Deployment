a106e2cd4d07c01ec6ef894f043314a06c5cc32c
SHUBHAM KUMAR SINGH <38123075+Singh07-Shubham@users.noreply.github.com>
2020-03-24 11:25:21 +0530

    Add files via upload

---
+++ Loan_API/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Loan_APIConfig(AppConfig):
+    name = 'Loan_API'
---
+++ Video_django/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class VideoDjangoConfig(AppConfig):
+    name = 'Video_django'
